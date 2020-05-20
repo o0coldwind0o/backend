ef755ba5206124cf893309637b6a7b538286fac0
o0coldwind0o <c.cfai@hotmail.com>
2020-05-20 18:18:44 +0800

    authorize handler

--- modules/authorizeHandlers.js
+++ modules/authorizeHandlers.js
@@ -2,7 +2,6 @@
 // only call next() to authorise this request if there is no error from the check
 
 'use strict'
-
 const authorize = require('./authorize')
 
 exports.authorize = (req, res, next) => {
